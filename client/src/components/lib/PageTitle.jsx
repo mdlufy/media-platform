6430927bfdb02276dec3e45d874d3cfbd269a208
mdlufy <german.panov.2018@mail.ru>
2022-09-23 23:41:00 +0300

    feat: add title hook

---
+++ client/src/components/lib/PageTitle.jsx
@@ -0,0 +1,29 @@
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import useDocumentTitle from "@tanem/use-document-title";
+
+function PageTitle(props) {
+    useDocumentTitle(`${props.title} | Media Platform`);
+    return (
+        <Grid
+            container
+            spacing={2}
+            mt={2}
+            mb={5}
+            style={{ alignItems: "center" }}
+        >
+            <Grid item xs={12} md="auto">
+                <Typography variant="h4" m={0}>
+                    {props.title}
+                </Typography>
+            </Grid>
+            {props.actionButton && (
+                <Grid item xs={12} md="auto" ml={"auto"}>
+                    {props.actionButton}
+                </Grid>
+            )}
+        </Grid>
+    );
+}
+
+export default PageTitle;

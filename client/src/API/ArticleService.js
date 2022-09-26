4f3d49e18bc6afcf34cca46db18775a9b145380c
mdlufy <german.panov.2018@mail.ru>
2022-09-26 18:56:25 +0300

    refactor: create separate axios instance for api call

--- client/src/API/ArticleService.js
+++ client/src/API/ArticleService.js
@@ -1,22 +1,21 @@
-import axios from "axios";
-import { API_URL } from "../components/articles/constants";
+import axios from "../axios.config";
 
 export default class ArticleService {
     static async getAll() {
-        const responce = await axios.get(API_URL);
+        const responce = await axios.get('/');
         
         return responce;
     }
 
     static async getById(id) {
-        const responce = await axios.get(`${API_URL}/${id}`);
+        const responce = await axios.get(`/${id}`);
         
         return responce;
 
     }
 
     static async deleteById(id) {
-        const responce = await axios.delete(`${API_URL}/${id}`);
+        const responce = await axios.delete(`/${id}`);
         
         return responce;
 

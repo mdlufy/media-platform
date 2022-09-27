1434e7baaa3bce41817e9e19c18d9723349fd824
mdlufy <german.panov.2018@mail.ru>
2022-09-27 11:40:16 +0300

    chore: refactor for deploy

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter basename="/media-platform">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <GlobalStyles />
         <ThemeProvider theme={theme}>
             <App />

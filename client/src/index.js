a1793c49d399d30780f3c7dfbfcb3c21e8b2d9df
mdlufy <german.panov.2018@mail.ru>
2022-09-27 11:09:19 +0300

    refactor: change basename for gh pages

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/media-platform">
         <GlobalStyles />
         <ThemeProvider theme={theme}>
             <App />

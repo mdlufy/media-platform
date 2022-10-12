d32c09a31c52cde31c679c5577b2ec2f06d8a3df
mdlufy <german.panov.2018@mail.ru>
2022-10-12 17:36:21 +0300

    feat: user, video, auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,15 +5,46 @@ import {MongooseModule} from '@nestjs/mongoose';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {ArticleService} from './articles/service/article.service';
+import {UserController} from './user/controller/user.controller';
+import {VideoController} from './video/controller/video.controller';
+import {JwtModule} from '@nestjs/jwt';
+import {secret} from './constants';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import {join} from 'path/posix';
+import {UserService} from './user/service/user.service';
+import {MulterModule} from '@nestjs/platform-express';
+import {diskStorage} from 'multer';
+import {v4 as uuidv4} from 'uuid';
 
 @Module({
     imports: [
         MongooseModule.forRoot('mongodb://localhost:27017', {
-            dbName: 'articledb',
+            dbName: 'Stream',
         }),
-        MongooseModule.forFeature([{name: 'Article', schema: ArticleSchema}]),
+        MulterModule.register({
+            storage: diskStorage({
+                destination: './public',
+                filename: (req, file, cb) => {
+                    const ext = file.mimetype.split('/')[1];
+                    cb(null, `${uuidv4()} - ${Date.now()}.${ext}`);
+                },
+            }),
+        }),
+        // MongooseModule.forFeature([{name: 'Article', schema: ArticleSchema}]),
+        JwtModule.register({
+            secret: secret,
+            signOptions: {expiresIn: '24h'},
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public'),
+        }),
     ],
-    controllers: [AppController, ArticleController],
-    providers: [AppService, ArticleService],
+    controllers: [
+        AppController,
+        ArticleController,
+        UserController,
+        VideoController,
+    ],
+    providers: [AppService, ArticleService, UserService],
 })
 export class AppModule {}

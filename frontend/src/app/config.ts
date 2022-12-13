a166a07999302a0d326420351c8701b965449284
mdlufy <german.panov.2018@mail.ru>
2022-12-13 15:00:52 +0300

    deploy

--- frontend/src/app/config.ts
+++
@@ -1,1 +0,0 @@
-export const apiUrl = 'http://localhost:3002/api/v1';
--- frontend/src/app/interfaces/video.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Video {
-    _id: string;
-    title: string;
-    coverImage: string;
-    video: string;
-    __v: number;
-}

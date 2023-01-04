d5bdeade8656a6f2517718fd6d61e4731b09fe47
mdlufy <german.panov.2018@mail.ru>
2023-01-04 18:34:01 +0300

    feat: update header design

--- frontend/src/app/pages/layout/layout.module.ts
+++ frontend/src/app/pages/layout/layout.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiModeModule, TuiSvgModule } from '@taiga-ui/core';
 import { TuiTabsModule } from '@taiga-ui/kit';
 import { LayoutComponent } from './layout.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     declarations: [LayoutComponent, NavbarComponent],
-    imports: [CommonModule, RouterModule, TuiTabsModule, TuiButtonModule],
+    imports: [
+        CommonModule,
+        RouterModule,
+        TuiTabsModule,
+        TuiButtonModule,
+        TuiSvgModule,
+        TuiModeModule,
+    ],
 })
 export class LayoutModule {}

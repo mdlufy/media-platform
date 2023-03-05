c186336293b00fb315051f448077cb801768bdbc
mdlufy <german.panov.2018@mail.ru>
2023-03-05 15:22:41 +0300

    refactor(auth): update logic of getting information from token to fetch user data

--- frontend/src/app/+state/profile/profile-load/profile-load.service.ts
+++ frontend/src/app/+state/profile/profile-load/profile-load.service.ts
@@ -1,31 +1,28 @@
-import { JwtToken } from './../../../interfaces/jwt';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { LOCAL_STORAGE } from '@ng-web-apis/common';
 import { Observable, of } from 'rxjs';
 import { JwtHelper } from 'src/app/helpers/jwt.helper';
+import { AccessTokenPayload } from 'src/app/interfaces/access-token';
 import { UserDto } from 'src/app/interfaces/user.dto';
 import { ACCESS_TOKEN } from 'src/app/jwt.interceptor';
 import { UserService } from './../../../api/user/user.service';
-import { SessionStorageService } from '../../../session-storage.service';
-import { AccessTokenPayload } from 'src/app/interfaces/access-token';
 
 @Injectable()
 export class ProfileLoadService {
     constructor(
+        @Inject(LOCAL_STORAGE) private readonly localStorageService: Storage,
         private userService: UserService,
-        private sessionStorageService: SessionStorageService
     ) {}
 
     public getProfile$(): Observable<UserDto | null> {
-        const accessToken = this.sessionStorageService.getItem(ACCESS_TOKEN);
+        const accessToken = this.localStorageService.getItem(ACCESS_TOKEN);
 
         if (!accessToken) {
             return of(null);
         }
 
-        const result = JwtHelper.getFromAccessToken<AccessTokenPayload>(accessToken);
+        const { email } = JwtHelper.getFromAccessToken<AccessTokenPayload>(accessToken);
 
-        console.log(result);
-
-        return this.userService.fetchUser$('mdlufy@me.');
+        return this.userService.fetchUser$(email);
     }
 }
